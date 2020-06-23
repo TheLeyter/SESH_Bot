1e077f12ba6af2f5fbf4e0bb74b80de3b67d0660
TheLeyter <59491306+TheLeyter@users.noreply.github.com>
2020-06-23 16:26:49 +0300

    Add files via upload

---
+++ Configuration/ConfigJson.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using Newtonsoft.Json;
+
+namespace DiscordBot
+{
+    struct ConfigJson
+    {
+        public string Token { get; set; }
+        public string Prefix { get; set; }
+    }
+    class Configuration
+    {
+        public ConfigJson conf { get; }
+        public Configuration()
+        {
+            string ConfigStr = File.ReadAllText("Config.json");
+            conf = JsonConvert.DeserializeObject<ConfigJson>(ConfigStr);
+        }
+    }
+}
---
+++ Handlers/CommandHandler.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using System.Reflection;
+
+namespace DiscordBot
+{
+    class CommandHandler
+    {
+
+        public string _prefix { get; set; }
+        private DiscordSocketClient _client;
+        private CommandService _commands;
+        private IServiceProvider _services;
+
+        public CommandHandler(IServiceProvider services,string prefix)
+        {
+            _commands = services.GetRequiredService<CommandService>();
+            _client = services.GetRequiredService<DiscordSocketClient>();
+            _services = services;
+            _prefix = prefix;
+            AddEvents();
+        }
+
+        public async Task InitializeAsync()
+        {
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
+        }
+
+        public void AddEvents()
+        {
+            _commands.Log += LogAsync;
+            _client.MessageReceived += HandleCommandAsync;
+        }
+
+        private async Task HandleCommandAsync(SocketMessage messageParam)
+        {
+            var message = messageParam as SocketUserMessage;
+            if (message == null)
+                return;
+
+            int prefPos = 0;
+
+            if (!(message.HasStringPrefix(_prefix, ref prefPos) || message.HasMentionPrefix(_client.CurrentUser, ref prefPos)) || message.Author.IsBot)
+                return;
+
+            var context = new SocketCommandContext(_client, message);
+
+            var result = await _commands.ExecuteAsync(context, prefPos, null);
+        }
+        private Task LogAsync(LogMessage log)
+        {
+            Console.WriteLine(log.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
